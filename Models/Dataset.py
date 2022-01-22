d852f0caeee7603492a6fb504a2abe06a71a330c
Christian Di Maio <dima9610@gmail.com>
2022-01-22 11:14:48 +0100

    Aggiunta classe Dataset State
    Un dataset puo` trovarsi in 3 stati:
    - Raw -> I Sample sono raw
    - Training -> I sample sono stati pre-processati per il training
    - Evaluation -> I sample sono stati pre-processati per la valutazione

--- Models/Dataset.py
+++ Models/Dataset.py
@@ -3,7 +3,22 @@ import os
 import pandas as pd 
 import torch
 import numpy as np 
+from enum import Enum
 
+
+class DatasetState(Enum):
+    """A dataset could be in 3 possible, mutual exclusive, state:
+        - Raw -> Sample are raw, no preprocessing operation performed
+        - Training -> All Samples are pre-processed for training
+        - Evaluation -> All Samples are pre-processed for evaluation
+
+    Args:
+        Enum (int): Raw or Training or Evaluation
+    """
+    Raw = 0
+    Training = 1
+    Evaluation = 2
+    
 # TO-Do
 # Aggiungere a README, la modalita` in cui si elabora il dataset e`: ho una cartella il cui contenuto e`: 
 #       1) un file result.csv che contiene i dati come il formato gia` definito
@@ -21,7 +36,7 @@ class Dataset():
     # sample is the <Sample> object associated 
     # dirty is boolean and it means: this sample was already taken from the method get_fraction_of_dataset, this implies that externally somebody already taken this sample.
     
-    def __init__(self, directory_of_data:str = None, already_computed_dataframe: pd.DataFrame = None):
+    def __init__(self, directory_of_data:str = None, already_computed_dataframe: pd.DataFrame = None, state: DatasetState = DatasetState.Raw):
         """Create a new dataset from source files
 
         Args:
@@ -38,8 +53,9 @@ class Dataset():
         
         _temp_dataset=pd.read_csv(f"{directory_of_data}/results.csv", sep="|", skipinitialspace=True)[["image_name","comment"]].iloc[0:1000,:]
         samples = _temp_dataset.apply(lambda row: Sample(int(row.name)+1,f"{directory_of_data}/images/{row.image_name}",row.comment),axis=1)
-        self.dataset = pd.DataFrame(list(zip([i for i in range(len(samples))],samples,[False for _ in range(len(samples))])), columns=["id_sample","sample","dirty"])
-    
+        
+        self.dataset: pd.DataFrame = pd.DataFrame(list(zip([i for i in range(len(samples))],samples,[False for _ in range(len(samples))])), columns=["id_sample","sample","dirty"])
+        self.state: DatasetState = state
     
     def suffle_data_set(self):
         self.dataset.apply(torch.randperm, axis=0)
